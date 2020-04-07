c1751ab6b5498b03b569cce7dae6d270b11208a4
luaifei <lu.aifei@thoughtworks.com>
2020-04-07 17:23:21 +0800

    Test GCS Operator

--- pull_data_by_api.py
+++ pull_data_by_api.py
@@ -2,12 +2,14 @@ import datetime
 import json
 
 import airflow
+from airflow.contrib.operators import file_to_gcs
 from airflow.models import Variable
 from airflow.operators import http_operator, python_operator
 
 YESTERDAY = datetime.datetime.now() - datetime.timedelta(days=1)
 USERNAME = Variable.get("username")
 PASSWORD = Variable.get("password")
+TABLE_LIST = ["sys_user", "cmn_location"]
 
 default_args = {
     'owner': 'Aifei Lu',
@@ -33,16 +35,24 @@ t1 = http_operator.SimpleHttpOperator(task_id="query_data",
                                       dag=dag)
 
 
-def handler(**context):
+def handle_response(**context):
     ti = context['ti']
     query_data = ti.xcom_pull(key=None, task_ids='query_data')
     if query_data:
-        print("Rec Query Data" + query_data)
+        with open("user.json", mode="w") as f:
+            f.write(query_data)
 
 
 t2 = python_operator.PythonOperator(task_id="handle_response",
-                                    python_callable=handler,
+                                    python_callable=handle_response,
                                     provide_context=True,
                                     dag=dag)
 
-t1.set_downstream(t2)
+
+t3 = file_to_gcs.FileToGoogleCloudStorageOperator(task_id="upload_raw_data",
+                                                  src="user.json",
+                                                  dst="data/user.json",
+                                                  bucket="asia-northeast1-example-env-c50e72d7-bucket")
+
+
+t1 >> t2 >> t3
