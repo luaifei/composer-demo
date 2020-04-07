431a15ae4fccf040f46be97dbe4db507ecd5d511
luaifei <lu.aifei@thoughtworks.com>
2020-04-07 15:23:19 +0800

    Test Airflow XCom

---
+++ pull_data_by_api.py
@@ -0,0 +1,45 @@
+import datetime
+
+import airflow
+from airflow.models import Variable
+from airflow.operators import http_operator, python_operator
+
+YESTERDAY = datetime.datetime.now() - datetime.timedelta(days=1)
+USERNAME = Variable.get("username")
+PASSWORD = Variable.get("password")
+
+default_args = {
+    'owner': 'Aifei Lu',
+    'depends_on_past': False,
+    'email': ['lu.aifei@thoughtworks.com'],
+    'email_on_failure': True,
+    'email_on_retry': False,
+    'start_date': YESTERDAY,
+}
+
+dag = airflow.DAG("pull_data_by_api",
+                  default_args=default_args,
+                  schedule_interval=datetime.timedelta(days=1))
+
+t1 = http_operator.SimpleHttpOperator(task_id="query_data",
+                                      endpoint="https://thoughtworksdev.service-now.com/api/now/table/sys_user",
+                                      data={"sysparm_limit": 10},
+                                      method="GET",
+                                      extra_options={"auth": (USERNAME, PASSWORD)},
+                                      xcom_push=True,
+                                      headers={"Content-Type": "application/json",
+                                               "Accept": "application/json"})
+
+
+def handler(**context):
+    ti = context['ti']
+    query_data = ti.xcom_pull(key=None, task_ids='query_data')
+    if query_data:
+        print("Rec Query Data" + query_data)
+
+
+t2 = python_operator.PythonOperator(task_id="handle_response",
+                                    python_callable=handler,
+                                    provide_context=True)
+
+t1.set_downstream(t2)
